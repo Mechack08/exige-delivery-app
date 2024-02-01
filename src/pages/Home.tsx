786d391e061d0c773e48ca9c111bd60cd5892b13
8pro <mechackkobius@gmail.com>
2024-02-01 10:27:30 +0300

    Implement Home structure

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="grid-rows-3">
+      <header>Header</header>
+      <main className="container">
+        <div>Main</div>
+      </main>
+      <div>Partners</div>
+      <footer>Footer</footer>
+    </div>
+  );
+}
