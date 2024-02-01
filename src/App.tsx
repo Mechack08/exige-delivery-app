5a1dc52bd91202e81c7a9240eef4edcba574bff1
8pro <mechackkobius@gmail.com>
2024-02-01 10:26:56 +0300

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import Home from "./pages/Home";
+
 export default function App() {
-  return (
-    <div className="container">
-      <h1>Hello world!</h1>
-    </div>
-  );
+  return <Home />;
 }
