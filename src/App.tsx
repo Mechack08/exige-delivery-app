f5995d12eddf57372637cab1dc2e356f98c9f9a0
8pro <mechackkobius@gmail.com>
2024-02-04 13:17:51 +0300

    Implement Router structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
 
 export default function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
 }
