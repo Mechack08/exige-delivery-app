149bcd3dc475bd5e79d0bd60508a170e48974319
8pro <mechackkobius@gmail.com>
2024-02-07 14:22:30 +0300

    Create Banner Layout

---
+++ src/layouts/Banner.tsx
@@ -0,0 +1,22 @@
+import { FaArrowRight } from "react-icons/fa6";
+
+import BannerImage from "../components/BannerImage";
+import BannerImage1 from "../assets/banner_images/image-1.png";
+import Button from "../components/Button";
+import BannerNextPrevBtns from "./BannerNextPrevBtns";
+import Dots from "../components/Dots";
+
+export default function Banner() {
+  return (
+    <div className="banner">
+      <BannerImage img={BannerImage1} alt="first pic to show" />
+      <span className="absolute-pos discover__more-btn">
+        <Button btn="Outlined" radius="Rounded" to="/">
+          Discover more <FaArrowRight />
+        </Button>
+      </span>
+      <Dots />
+      <BannerNextPrevBtns />
+    </div>
+  );
+}
