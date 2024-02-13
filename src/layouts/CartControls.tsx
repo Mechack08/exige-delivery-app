5e5c7b3f1714c72fa7a7a83bdbec6d56197254ad
8pro <mechackkobius@gmail.com>
2024-02-13 06:37:08 +0300

    Refactoring | get increment and decrement dispatch from Redux

--- src/layouts/CartControls.tsx
+++ src/layouts/CartControls.tsx
@@ -1,15 +1,33 @@
-import { Order } from "./OrderItems";
+import { useDispatch } from "react-redux";
+import {
+  Order,
+  decrementMenu,
+  incrementMenu,
+} from "../features/cart/cartSlice";
+import { useEffect, useState } from "react";
 
 type CartControlsProps = {
+  id: Order["id"];
   qty: Order["quantity"];
 };
 
-export default function CartControls({ qty }: CartControlsProps) {
+export default function CartControls({ id, qty }: CartControlsProps) {
+  const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    setQuantity(qty);
+  }, [qty]);
+
   return (
     <span className="cart__controls">
-      <button>-</button>
-      <input type="text" defaultValue={qty} />
-      <button>+</button>
+      <button onClick={() => dispatch(decrementMenu(id))}>-</button>
+      <input
+        type="text"
+        value={quantity}
+        onChange={(e) => setQuantity(+e.target.value)}
+      />
+      <button onClick={() => dispatch(incrementMenu(id))}>+</button>
     </span>
   );
 }
