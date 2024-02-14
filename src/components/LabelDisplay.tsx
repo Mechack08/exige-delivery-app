ef824825294d228ca33f53b9198abe4a1685a36d
8pro <mechackkobius@gmail.com>
2024-02-14 21:49:35 +0300

    Refactoring

--- src/components/LabelDisplay.tsx
+++ src/components/LabelDisplay.tsx
@@ -1,14 +1,22 @@
-import { type ReactNode } from "react";
 import { IoMdClose } from "react-icons/io";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../store/store";
+import { removeSurplus } from "../features/cart/cartSlice";
 
 type LabelDisplayProps = {
-  children: ReactNode;
+  item: string;
+  id: number;
 };
 
-export default function LabelDisplay({ children }: LabelDisplayProps) {
+export default function LabelDisplay({ item, id }: LabelDisplayProps) {
+  const dispatch: AppDispatch = useDispatch();
+
   return (
-    <li className="display__item">
-      {children}
+    <li
+      className="display__item"
+      onClick={() => dispatch(removeSurplus({ id, surplus: item }))}
+    >
+      {item}
       <IoMdClose />
     </li>
   );
