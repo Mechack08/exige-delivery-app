0640683c519fcd7c0c2b08dabb6a793c44e8a197
8pro <mechackkobius@gmail.com>
2024-02-20 08:05:44 +0300

    add Article Section Component to the main page

--- src/layouts/Main.tsx
+++ src/layouts/Main.tsx
@@ -1,9 +1,11 @@
+import ArticlesSection from "./ArticlesSection";
 import Banner from "./Banner";
 
 export default function Main() {
   return (
     <main>
       <Banner />
+      <ArticlesSection />
     </main>
   );
 }
