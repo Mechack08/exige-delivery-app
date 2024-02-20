98e639d8cab282cbd91abfcf60900f6030da6ad2
8pro <mechackkobius@gmail.com>
2024-02-20 07:52:06 +0300

    Artticle Section UI created

---
+++ src/layouts/ArticlesSection.tsx
@@ -0,0 +1,11 @@
+import ArticleSectionHeader from "../components/ArticleSectionHeader";
+import CardReastaurantList from "../components/CardReastaurantList";
+
+export default function ArticlesSection() {
+  return (
+    <div className="article__section">
+      <ArticleSectionHeader />
+      <CardReastaurantList />
+    </div>
+  );
+}
