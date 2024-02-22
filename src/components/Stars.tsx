3864275adec790e0ef828283c4f7772c7a34722d
8pro <mechackkobius@gmail.com>
2024-02-22 07:26:19 +0300

    Refactore Stars COmponent

--- src/components/Stars.tsx
+++ src/components/Stars.tsx
@@ -1,11 +1,13 @@
 import { FaStar } from "react-icons/fa";
 import { useState } from "react";
 
-export default function Stars() {
+type StarsProps = { maxStar: number };
+
+export default function Stars({ maxStar }: StarsProps) {
   const [rating, setRating] = useState<number>(0);
   const [hover, setHover] = useState<number>(0);
 
-  const LENGTH = 5;
+  const LENGTH = maxStar;
 
   return (
     <div className="star__list">
@@ -26,7 +28,7 @@ export default function Stars() {
             <input
               type="radio"
               name="rating"
-              value={ratingValue}
+              value={rating}
               onChange={() => setRating(ratingValue)}
               style={{ display: "none" }}
             />
